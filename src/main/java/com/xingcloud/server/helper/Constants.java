80e8948908ab6f1b089922a7924eb2e9fd08cad4
IvyTang <ivytang0812@gmail.com>
2013-08-06 14:01:19 +0800

    120w

--- src/main/java/com/xingcloud/server/helper/Constants.java
+++ src/main/java/com/xingcloud/server/helper/Constants.java
@@ -39,7 +39,7 @@ public class Constants {
 
   public static int USER_ONCE_READ = 3 * 10000;
 
-  public static int USER_BULK_LOAD_ONCE_READ = 63 * 10000;
+  public static int USER_BULK_LOAD_ONCE_READ = 120 * 10000;
 
   public static int EXECUTOR_THREAD_COUNT = 30;
 
