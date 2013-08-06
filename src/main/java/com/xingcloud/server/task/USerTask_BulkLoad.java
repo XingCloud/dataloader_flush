b1d107309dc1962fd5fa659c4a251506097b819f
IvyTang <ivytang0812@gmail.com>
2013-08-06 14:53:28 +0800

    bulk load wait 100min

--- src/main/java/com/xingcloud/server/task/USerTask_BulkLoad.java
+++ src/main/java/com/xingcloud/server/task/USerTask_BulkLoad.java
@@ -225,7 +225,7 @@ public class USerTask_BulkLoad implements Runnable {
 
     for(Future<Boolean> booleanFuture:futures){
       try {
-        booleanFuture.get(30,TimeUnit.MINUTES);
+        booleanFuture.get(100,TimeUnit.MINUTES);
       } catch (ExecutionException e) {
         LOG.error(e.getMessage(),e);
       } catch (TimeoutException e) {
