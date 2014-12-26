091af1fe9086a7a1ebf41b80a551f3b12db8547e
griffinl <lq0987@126.com>
2014-12-26 10:38:04 +0800

    update

--- src/main/java/com/xingcloud/server/helper/Constants.java
+++ src/main/java/com/xingcloud/server/helper/Constants.java
@@ -41,7 +41,7 @@ public class Constants {
 
   public static int USER_BULK_LOAD_ONCE_READ = 500 * 10000;
 
-  public static int EXECUTOR_THREAD_COUNT = 5;
+  public static int EXECUTOR_THREAD_COUNT = 6;
 
   public static int DELAY_ONCE_READ = 10000;
 
