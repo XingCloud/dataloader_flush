01750fe1af632ed26344afd5c6d7933e1b21d514
griffinl <lq0987@126.com>
2014-12-29 14:09:32 +0800

    update

--- src/main/java/com/xingcloud/server/helper/Constants.java
+++ src/main/java/com/xingcloud/server/helper/Constants.java
@@ -41,7 +41,7 @@ public class Constants {
 
   public static int USER_BULK_LOAD_ONCE_READ = 500 * 10000;
 
-  public static int EXECUTOR_THREAD_COUNT = 3;
+  public static int EXECUTOR_THREAD_COUNT = 2;
 
   public static int DELAY_ONCE_READ = 10000;
 
@@ -72,7 +72,7 @@ public class Constants {
   public static final int OFFLINE_DB = 15;
 
 
-  public static final int MYSQL_BL_THREADS = 16;
+  public static final int MYSQL_BL_THREADS = 24;
 
   public static final int HBASE_FLUSH_THREADS = 16;
 
