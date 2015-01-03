e15d128d6806ef14bf4cbfd3e8cb51d01c89607a
liqiang <lq0987@126.com>
2015-01-03 17:01:52 +0800

    加类型判断

--- src/main/java/com/xingcloud/server/task/UserTaskBulkLoadV3.java
+++ src/main/java/com/xingcloud/server/task/UserTaskBulkLoadV3.java
@@ -51,7 +51,7 @@ public class UserTaskBulkLoadV3 implements Runnable {
             LOG.info("enter user task. user size:" + users.size() + "\tproject: " + project);
 
             // node, rowkey, value
-            Map<String, Map<UpdateFunc, Map<byte[], String>>> nodeTableSBMap = new HashMap<String, Map<UpdateFunc, Map<byte[], String>>>();
+            Map<String, Map<UpdateFunc, Map<byte[], Object>>> nodeTableSBMap = new HashMap<String, Map<UpdateFunc, Map<byte[], Object>>>();
 
             long startTime = System.currentTimeMillis();
             prepareUsers(nodeTableSBMap);
@@ -82,7 +82,7 @@ public class UserTaskBulkLoadV3 implements Runnable {
      *
      * @param nodeTableSBMap
      */
-    private void prepareUsers(Map<String, Map<UpdateFunc, Map<byte[], String>>> nodeTableSBMap) throws Exception {
+    private void prepareUsers(Map<String, Map<UpdateFunc, Map<byte[], Object>>> nodeTableSBMap) throws Exception {
         // sort List<User_BulkLoad> users, in user hash uid order.
         Collections.sort(users, new UserComparator());
 
@@ -102,23 +102,36 @@ public class UserTaskBulkLoadV3 implements Runnable {
                     continue;
                 }
 
-                Map<UpdateFunc, Map<byte[], String>> lists = nodeTableSBMap.get(nodeAddress);
+                Map<UpdateFunc, Map<byte[], Object>> lists = nodeTableSBMap.get(nodeAddress);
                 if (lists == null) {
-                    lists = new HashMap<UpdateFunc, Map<byte[], String>>();
-                    lists.put(UpdateFunc.inc, new HashMap<byte[], String>());
-                    lists.put(UpdateFunc.cover, new HashMap<byte[], String>());
-                    lists.put(UpdateFunc.once, new HashMap<byte[], String>());
+                    lists = new HashMap<UpdateFunc, Map<byte[], Object>>();
+                    lists.put(UpdateFunc.inc, new HashMap<byte[], Object>());
+                    lists.put(UpdateFunc.cover, new HashMap<byte[], Object>());
+                    lists.put(UpdateFunc.once, new HashMap<byte[], Object>());
                 }
-                lists.get(userProp.getPropFunc()).put(createRowKey(project, key, user.getSamplingUid()), value);
+
+                Object result = value;
+                switch (userProp.getPropType()) {
+                    case sql_bigint:
+                        result = Long.parseLong(value);
+                        break;
+                    case sql_datetime:
+                        result = Long.parseLong(value);
+                        break;
+                    case sql_string:
+
+                }
+
+                lists.get(userProp.getPropFunc()).put(createRowKey(project, key, user.getSamplingUid()), result);
             }
         }
     }
 
 
-    private void bulkLoad(Map<String, Map<UpdateFunc, Map<byte[], String>>> nodeTableSBMap) throws InterruptedException {
+    private void bulkLoad(Map<String, Map<UpdateFunc, Map<byte[], Object>>> nodeTableSBMap) throws InterruptedException {
 
         List<Future<Boolean>> futures = new ArrayList<Future<Boolean>>();
-        for (Map.Entry<String, Map<UpdateFunc, Map<byte[], String>>> entry : nodeTableSBMap.entrySet()) {
+        for (Map.Entry<String, Map<UpdateFunc, Map<byte[], Object>>> entry : nodeTableSBMap.entrySet()) {
             UserChildThread userChildThread = new UserChildThread(entry.getKey(), project, entry.getValue());
             futures.add(MySQLBulkLoadExecutor.getInstance().submit(userChildThread));
         }
@@ -140,9 +153,9 @@ class UserChildThread implements Callable<Boolean> {
 
     private String node;
     private String project;
-    private Map<UpdateFunc, Map<byte[], String>> attrs;
+    private Map<UpdateFunc, Map<byte[], Object>> attrs;
 
-    public UserChildThread(String node, String project, Map<UpdateFunc, Map<byte[], String>> attrs) {
+    public UserChildThread(String node, String project, Map<UpdateFunc, Map<byte[], Object>> attrs) {
         this.node = node;
         this.project = project;
         this.attrs = attrs;
@@ -150,7 +163,7 @@ class UserChildThread implements Callable<Boolean> {
 
     public Boolean call() {
         int count = 0;
-        for (Map.Entry<UpdateFunc, Map<byte[], String>> users : attrs.entrySet()) {
+        for (Map.Entry<UpdateFunc, Map<byte[], Object>> users : attrs.entrySet()) {
             count += users.getValue().size();
             while (true) {
                 HTable table = null;
@@ -158,10 +171,16 @@ class UserChildThread implements Callable<Boolean> {
                 try {
 
                     List<Put> puts = new ArrayList<Put>();
-                    for (Map.Entry<byte[], String> user : users.getValue().entrySet()) {
+                    for (Map.Entry<byte[], Object> user : users.getValue().entrySet()) {
                         Put put = new Put(user.getKey());
                         put.setWriteToWAL(Constants.deuTableWalSwitch);
-                        put.add(Constants.userColumnFamily.getBytes(), Constants.userColumnFamily.getBytes(), Bytes.toBytes(user.getValue()));
+                        byte[] value = null;
+                        if(user.getValue() instanceof Long){
+                            value = Bytes.toBytes((Long)user.getValue());
+                        }else{
+                            value = Bytes.toBytes((String)user.getValue());
+                        }
+                        put.add(Constants.userColumnFamily.getBytes(), Constants.userColumnFamily.getBytes(),value);
                         puts.add(put);
                     }
 
@@ -177,8 +196,8 @@ class UserChildThread implements Callable<Boolean> {
                             table.checkAndPut(put.getRow(), Constants.userColumnFamily.getBytes(), Constants.userColumnFamily.getBytes(), null, put);
                         }
                     } else if (users.getKey() == UpdateFunc.inc) {
-                        for (Map.Entry<byte[], String> user : users.getValue().entrySet()) {
-                            table.incrementColumnValue(user.getKey(), Constants.userColumnFamily.getBytes(), Constants.userColumnFamily.getBytes(), Long.parseLong(user.getValue()), false);
+                        for (Map.Entry<byte[], Object> user : users.getValue().entrySet()) {
+                            table.incrementColumnValue(user.getKey(), Constants.userColumnFamily.getBytes(), Constants.userColumnFamily.getBytes(), (Long)user.getValue(), false);
                         }
                     }
 
