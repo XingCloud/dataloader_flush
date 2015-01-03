3d3f17906a2619fadd7a14a67b1f44dcb41fc254
liqiang <lq0987@126.com>
2015-01-03 21:36:14 +0800

    工具

---
+++ src/main/java/com/xingcloud/server/helper/HBaseTool.java
@@ -0,0 +1,37 @@
+package com.xingcloud.server.helper;
+
+import com.xingcloud.server.task.HBaseConf;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.MasterNotRunningException;
+import org.apache.hadoop.hbase.ZooKeeperConnectionException;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.io.hfile.Compression;
+import org.apache.hadoop.hbase.regionserver.StoreFile;
+
+import java.io.IOException;
+
+
+/**
+ * Created by Administrator on 2015/1/3.
+ */
+public class HBaseTool {
+
+    public static void main(String[] args) throws IOException {
+        HTableDescriptor tableDescriptor = new HTableDescriptor("user_attribute");
+
+        HColumnDescriptor column = new HColumnDescriptor("v");
+        column.setCompressionType(Compression.Algorithm.LZO);
+        column.setMaxVersions(1);
+        column.setBloomFilterType(StoreFile.BloomType.ROWCOL);
+        tableDescriptor.addFamily(column);
+
+        for(int i=0;i<=15;i++){
+            String node = "node" + i;
+            Configuration conf = HBaseConf.getInstance().getHBaseConf(node);
+            HBaseAdmin admin = new HBaseAdmin(conf);
+            admin.createTable(tableDescriptor);
+        }
+    }
+}
