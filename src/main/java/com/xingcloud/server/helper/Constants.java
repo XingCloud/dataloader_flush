4d6bc6bdfe565415f97da48d645069c38ca88414
griffinl <lq0987@126.com>
2015-01-15 23:47:36 +0800

    update

--- src/main/java/com/xingcloud/server/helper/Constants.java
+++ src/main/java/com/xingcloud/server/helper/Constants.java
@@ -72,7 +72,7 @@ public class Constants {
   public static final int OFFLINE_DB = 15;
 
 
-  public static final int MYSQL_BL_THREADS = 20;
+  public static final int MYSQL_BL_THREADS = 12;
 
   public static final int HBASE_FLUSH_THREADS = 16;
 
