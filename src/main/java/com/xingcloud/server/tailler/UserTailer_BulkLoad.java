59f0a09d717073ffe840e6641a2e9f54bf0d7fe4
griffinl <lq0987@126.com>
2015-01-16 10:28:36 +0800

    update

--- src/main/java/com/xingcloud/server/tailler/UserTailer_BulkLoad.java
+++ src/main/java/com/xingcloud/server/tailler/UserTailer_BulkLoad.java
@@ -60,7 +60,7 @@ public class UserTailer_BulkLoad extends Tail {
             }
         });
 
-        int headCount = 40;
+        int headCount = 42;
 
         int headLength = entryList.size() > headCount ? headCount: entryList.size();
         int tailLength = entryList.size() > headCount ? entryList.size() - headCount : 0;
